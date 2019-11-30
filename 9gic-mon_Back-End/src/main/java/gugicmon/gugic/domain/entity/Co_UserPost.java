78b70ed16962e2588e77c69ddbbef147a09cf4c6
iopasd753951 <iopasd753951@gmail.com>
2019-11-30 20:57:06 +0900

    [Add] entity 생성자, column 추가

--- 9gic-mon_Back-End/src/main/java/gugicmon/gugic/domain/entity/Co_UserPost.java
+++ 9gic-mon_Back-End/src/main/java/gugicmon/gugic/domain/entity/Co_UserPost.java
@@ -3,6 +3,7 @@ package gugicmon.gugic.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Cleanup;
 import lombok.extern.apachecommons.CommonsLog;
 
 import javax.persistence.*;
@@ -22,17 +23,12 @@ public class Co_UserPost {
     @Column(name = "title")
     String title;
 
-    @JsonFormat(pattern="yyyy-MM-dd")
     @Column(name = "dead_line")
-    Date deadLine;
+    String deadLine;
 
-//    @OneToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "co_user_post_copname", referencedColumnName = "co_user_copname")
     @Column(name = "co_user_post_copname")
     String coUserPostCopname;
 
-//    @OneToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "co_user_image_url", referencedColumnName = "co_user_post_imageurl")
     @Column(name = "co_user_post_image_url")
     String coUserPostImageUrl;
 
@@ -54,4 +50,181 @@ public class Co_UserPost {
     @Column(name = "etc")
     String etc;
 
+    @Column(name = "background_image_url")
+    String backgroundImageUrl;
+
+    @Column(name = "grade")
+    int grade;
+
+    @Column(name = "subject")
+    String subject;
+
+    @Column(name = "region")
+    String region;
+
+    @JsonIgnore
+    @Column(name = "grade_id")
+    int gradeId = 0;
+
+    @JsonIgnore
+    @Column(name = "subject_id")
+    String subjectId = "0";
+
+    @JsonIgnore
+    @Column(name = "region_id")
+    String regionId = "0";
+
+    public Long getIdx() {
+        return idx;
+    }
+
+    public void setIdx(Long idx) {
+        this.idx = idx;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(String deadLine) {
+        this.deadLine = deadLine;
+    }
+
+    public String getCoUserPostCopname() {
+        return coUserPostCopname;
+    }
+
+    public void setCoUserPostCopname(String coUserPostCopname) {
+        this.coUserPostCopname = coUserPostCopname;
+    }
+
+    public String getCoUserPostImageUrl() {
+        return coUserPostImageUrl;
+    }
+
+    public void setCoUserPostImageUrl(String coUserPostImageUrl) {
+        this.coUserPostImageUrl = coUserPostImageUrl;
+    }
+
+    public String getSimpleIntro() {
+        return simpleIntro;
+    }
+
+    public void setSimpleIntro(String simpleIntro) {
+        this.simpleIntro = simpleIntro;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public String getMainWork() {
+        return mainWork;
+    }
+
+    public void setMainWork(String mainWork) {
+        this.mainWork = mainWork;
+    }
+
+    public String getMainWorkStack() {
+        return mainWorkStack;
+    }
+
+    public void setMainWorkStack(String mainWorkStack) {
+        this.mainWorkStack = mainWorkStack;
+    }
+
+    public String getWelfare() {
+        return welfare;
+    }
+
+    public void setWelfare(String welfare) {
+        this.welfare = welfare;
+    }
+
+    public String getEtc() {
+        return etc;
+    }
+
+    public void setEtc(String etc) {
+        this.etc = etc;
+    }
+
+    public String getBackgroundImageUrl() {
+        return backgroundImageUrl;
+    }
+
+    public void setBackgroundImageUrl(String backgroundImage) {
+        this.backgroundImageUrl = backgroundImageUrl;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public Co_UserPost() {
+        super();
+    }
+
+    public Co_UserPost(String title, String deadLine, String coUserPostCopname, String coUserPostImageUrl, String simpleIntro, String context, String mainWork, String mainWorkStack, String welfare, String etc, String backgroundImageUrl, int grade, String subject, String region) {
+        super();
+        this.title = title;
+        this.deadLine = deadLine;
+        this.coUserPostCopname = coUserPostCopname;
+        this.coUserPostImageUrl = coUserPostImageUrl;
+        this.simpleIntro = simpleIntro;
+        this.context = context;
+        this.mainWork = mainWork;
+        this.mainWorkStack = mainWorkStack;
+        this.welfare = welfare;
+        this.etc = etc;
+        this.backgroundImageUrl = backgroundImageUrl;
+        this.grade = grade;
+        this.subject = subject;
+        this.region = region;
+    }
+
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public String getRegionId() {
+        return regionId;
+    }
 }
