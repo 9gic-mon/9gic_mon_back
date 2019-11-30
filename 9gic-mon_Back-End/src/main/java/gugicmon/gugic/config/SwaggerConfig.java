7a5db8fa341fc9f83b934d36dfe4039c9efff053
iopasd753951 <iopasd753951@gmail.com>
2019-11-30 20:51:36 +0900

    [ADD] MVC관련추가

--- 9gic-mon_Back-End/src/main/java/gugicmon/gugic/config/SwaggerConfig.java
+++ 9gic-mon_Back-End/src/main/java/gugicmon/gugic/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package gugicmon.gugic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,8 +14,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
-
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+@Override
+public void addResourceHandlers(ResourceHandlerRegistry registry){
+    registry.addResourceHandler("/static/**")
+            .addResourceLocations("classpath:/static/");
+    registry.addResourceHandler("swagger-ui.html") .
+            addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("/webjars/**") .
+            addResourceLocations("classpath:/META-INF/resources/webjars/");
+}
 //swagger-ui.html#/
 @Bean
 public Docket api(){
